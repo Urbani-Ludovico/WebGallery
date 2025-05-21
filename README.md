c92895b627aedd383efe6a127a83d29965b10f95
Ludovico Urbani <lu.urbani@outlook.com>
2025-05-21 11:37:27 +0200

    Express configuration .env file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-
 # Web Gallery
+
+## Install
+
+### .env file
+
+```.dotenv
+DEBUG=1
+PORT=3000
+BODY_MAX_SIZE=128000000
+```
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,59 @@
 import express from "express";
+import compression from "compression";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import createMemoryStore from "memorystore";
+import timeout from "connect-timeout";
 
+
+dotenv.config()
 
 const port: number | string = process.env.PORT || 3000;
-const debug: boolean = process.argv.slice(2).includes("--debug");
+const debug: boolean = process.env.DEBUG === "1";
 
 const app: express.Application = express();
+
+
+// -------------------- APP CONFIG --------------------
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use(compression());
+
+app.disable("x-powered-by");
+
+app.use(express.json({
+    limit: process.env.BODY_MAX_SIZE + "B"
+}));
+app.use(express.urlencoded({
+    limit: process.env.BODY_MAX_SIZE + "B",
+    extended: true
+}));
+
+app.use(cookieParser());
+
+const MemoryStore = createMemoryStore(session);
+
+app.use(session({
+    secret: [
+        "y?UWOR}a=im8XL?}[YE)@TV$&'qF8iQ}L0J7+5N%Q>dx7EE'c:e7x,v*)~lc2YO", "_-lQd>_!t|[!d_(raN8702^]P0~aywIN%IwOFmQ7YZ3CV@)Z^S-Y'lGIGJ*;d69", "3e2*FI*eb{U:$hBs[8B,stnry3x8Y?>OT[o)u$Hs{>G=9d{;ox(u9tnu.r9syl%", "DYJ&{o(H_Xmqv+y:Azpqn=bk|R;uZIsaMB>W'-mD2y$KbT'K.0Y*Beblb<{;QaQ", "Yp~5oA)X.w#fBF]Tum'q%iuQ7%SGCL:gMB!;oMwlH7r*_1HI}4RaJEV'Ze]lQEX"
+    ],
+    store: new MemoryStore({
+        checkPeriod: 12 * 60 * 60 * 1000
+    }),
+    name: "session",
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 2 * 60 * 60 * 1000,
+        secure: process.env.NODE_ENV === "production",
+        httpOnly: true
+    }
+}));
+
+app.use(timeout(15 * 60 * 1000));
 
 // -------------------- RUN --------------------
 
